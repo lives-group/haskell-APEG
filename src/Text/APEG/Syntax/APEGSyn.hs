253dfc0e12cbb186a63a3c0140360be40aa98209
Rodrigo <rodrigogribeiro@gmail.com>
2016-05-16 20:45:36 -0300

    Some progresses

--- src/Text/APEG/Syntax/APEGSyn.hs
+++ src/Text/APEG/Syntax/APEGSyn.hs
@@ -2,9 +2,6 @@
 
 module Text.APEG.Syntax.APEGSyn where
 
-import Control.Applicative
-import Control.Monad
-
 import Data.Proxy    
 import Data.Singletons.Prelude   
 import Data.Singletons.Prelude.List
@@ -22,32 +19,15 @@ data APEG (env :: [(Symbol,*)]) (a :: *) where
   Success :: a -> APEG env a       
   Failure :: APEG env a
   Not :: APEG env a -> APEG env ()
-  Cat :: APEG env (a -> b) -> APEG env a -> APEG env b
+  Cat :: APEG env (a -> b) -> APEG env' a -> APEG (env :++ env') b
   Choice :: APEG env a -> APEG env a -> APEG env a
   Star :: APEG env a -> APEG env [a]
   Map :: (a -> b) -> APEG env a -> APEG env b
   Atrib :: KnownSymbol s => proxy s -> b -> APEG ('(s,b) ': '[]) ()
   Get :: KnownSymbol s => proxy s -> APEG env (s `At` env)       
-  Bind :: APEG env a -> (a -> APEG env' b) -> APEG (env' :++ env) b       
 
 instance Functor (APEG env) where
     fmap = Map
-
-instance Applicative (APEG env) where
-    pure = Success
-    (<*>) = Cat
-
-instance Alternative (APEG env) where
-    (<|>) = Choice
-    empty = Failure
-
--- instance Monad (APEG env) where
---     return = Success
---     (>>=) = Bind
-
--- instance MonadPlus (APEG env) where
---     mplus = (<|>)
---     mzero = empty        
             
 chr :: Char -> APEG env Char
 chr = Chr
@@ -64,7 +44,7 @@ star p = Star p
 string :: String -> APEG env String
 string = Symb          
 
-atrib :: KnownSymbol s => Proxy s -> b -> APEG ('(s,b) ': '[]) ()
+atrib :: KnownSymbol s => proxy s -> b -> APEG ('(s,b) ': '[]) ()
 atrib = Atrib         
 
 get :: KnownSymbol s => proxy s -> APEG env (s `At` env)
@@ -73,9 +53,15 @@ get = Get
 check :: KnownSymbol s => proxy s -> ((s `At` env) -> Bool) -> APEG env Bool
 check s f = f <$> Get s         
 
-mthen :: APEG env a -> APEG env' b -> APEG (env' :++ env) b
-mthen p q = Bind p (\_ -> q)         
+infixl 4 <$$>
+infixl 4 <**>
+            
+(<$$>) :: (a -> b) -> APEG env a -> APEG env b
+f <$$> p = Map f p          
 
+(<**>) :: APEG env (a -> b) -> APEG env' a -> APEG (env :++ env') b
+p <**> q = Cat p q          
+           
 -- simple tests
             
 foo :: APEG ('("a", Bool) ': '[]) ()
@@ -83,14 +69,6 @@ foo = Atrib (Proxy :: Proxy "a") True
 
 foo' :: APEG ('("b", Char) ': '[]) ()
 foo' = atrib (Proxy :: Proxy "b") 'a'        
-      
-faa :: APEG ('("b", Char) ': '("a", Bool) ': '[]) Char
-faa = foo `mthen` foo' `mthen` get (Proxy :: Proxy "b")
 
-
-    -- Bind foo
-    --         (Bind (Atrib (Proxy :: Proxy "b") (\_ -> pure True))
-    --               (get (Proxy :: Proxy "a")))
-           
-           
-hh = \ (s :: String) -> Proxy :: Proxy s           
+mytest = Cat (Map (\ _ y -> y) (Cat (Map (\ _ y -> y) foo) foo')) (Get (Proxy :: Proxy "b"))        
+mytest1 = (\_ _ c -> c) <$$> foo <**> foo' <**> get (Proxy :: Proxy "b")
